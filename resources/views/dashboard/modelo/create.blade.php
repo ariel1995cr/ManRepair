df0c8e5c01952f6a81ae771e7c4abb3b5cd5f615
Sergio ariel triviño <arielrnr1995@gmail.com>
2021-06-17 12:31:42 -0300

    fix doble mensaje

--- resources/views/dashboard/modelo/create.blade.php
+++ resources/views/dashboard/modelo/create.blade.php
@@ -10,7 +10,6 @@
 
             <h1 class="display-1 text-center">Crear Modelo</h1>
             <hr>
-            @include('dashboard.vistasParciales.create-exitosa')
             <div class="row justify-content-center align-items-center">
                 <div class="col-12 col-md-6">
                     @include('dashboard.modelo.formularioModelo')
