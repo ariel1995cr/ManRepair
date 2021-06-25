b3162923615d25ec60e3613f9a6d966b978861c5
Sergio ariel triviño <arielrnr1995@gmail.com>
2021-06-24 21:04:52 -0300

    fix cascade update orden de servicio

--- database/migrations/2021_05_15_040736_create_celular_table.php
+++ database/migrations/2021_05_15_040736_create_celular_table.php
@@ -19,8 +19,8 @@ class CreateCelularTable extends Migration
             $table->string('nombre_modelo');
             $table->timestamps();
 
-            $table->foreign('nombre_marca')->references('nombre')->on('marca');
-            $table->foreign('nombre_modelo')->references('nombre')->on('modelo');
+            $table->foreign('nombre_marca')->references('nombre')->on('marca')->onUpdate('cascade');
+            $table->foreign('nombre_modelo')->references('nombre')->on('modelo')->onUpdate('cascade');
 
             $table->primary(['imei', 'nombre_marca', 'nombre_modelo']);
         });
